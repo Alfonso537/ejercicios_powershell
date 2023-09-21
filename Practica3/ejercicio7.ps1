beb12733acb2ea717f556ad36e3e7ee8a2082cf9
Alfonso <alfonsocastellperiz@gmail.com>
2023-09-21 12:07:07 +0200

    Practica3

---
+++ Practica3/ejercicio7.ps1
@@ -0,0 +1,7 @@
+﻿[string] $password = Read-Host "Introduce la contraseña"
+$passwordok = "alfonso"
+if ($password -ieq $passwordok) {
+    Write-Host "La contraseña es correcta"
+}else{
+    Write-Host "La contraseña es incorrecta"
+}
