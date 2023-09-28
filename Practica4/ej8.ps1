3eaab5aabedd93813835dad1b432783fd415ddb4
Alfonso <alfonsocastellperiz@gmail.com>
2023-09-28 08:26:48 +0200

    Ejercicios Practica4

--- Practica4/ej8.ps1
+++ Practica4/ej8.ps1
@@ -1,17 +1,12 @@
-﻿[int] $num = Read-Host "Dame un número"
-$fila = ""
+﻿[int]$num = Read-Host "Dame un número"
 
 for ($i = 1; $i -le $num; $i++) {
-   $row = @() 
-   for ($j = $i; $j -gt 0; $j--) {
-        if ($j % 2 -eq 1){
-            $row += $j
-            
-        }   
-   }
-        $filaactual = $row -join ' '
-        if ($filaactual -ne $fila) {
-            Write-Host $filaactual
-            $fila = $filaactual
+
+    [string]$res = ""
+
+    for ($j = 2*$i-1; $j -ge 1; $j-=2 ) {
+       
+        $res += "$j "
     }
+    Write-Host "$res"
 }
