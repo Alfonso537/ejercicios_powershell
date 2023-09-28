2312418c01602458829d6390868aa37b56c8673f
Alfonso <alfonsocastellperiz@gmail.com>
2023-09-28 09:23:21 +0200

    Ejercicios extra

---
+++ Practica4/extra1.ps1
@@ -0,0 +1,12 @@
+﻿[int]$num = Read-Host "Dame un número"
+
+for ($i = 0; $i -lt $num; $i++) {
+
+    [string]$res = ""
+
+    for ($j = $i+1; $j -ge 1; $j-=1 ) {
+       
+        $res += "$j "
+    }
+    Write-Host "$res"
+}
---
+++ Practica4/extra2.ps1
@@ -0,0 +1,17 @@
+﻿[int]$num = Read-Host "Dame un número"
+
+for ($i = 0; $i -lt $num; $i++) {
+
+    [string]$res = ""
+
+    for ($j = $i+1; $j -ge 1; $j-=1 ) {
+
+        if ($j -eq 1) {
+       
+        $res += "*"
+        }else{
+        $res += "-"
+        }
+    }
+    Write-Host "$res"
+}
