18f05fa47c56e1f6576ac8499279a69d6359e62e
Alfonso <alfonsocastellperiz@gmail.com>
2023-10-02 09:08:50 +0200

    Practica5

---
+++ Practica5/ej1.ps1
@@ -0,0 +1,76 @@
+﻿Write-Host "****** CALCULADORA ********
+
+    1.Sumar
+    2.Restar
+    3.Multiplicar
+    4.Dividir
+    5.Salir
+
+¿Qué desea hacer?Elige una opción:"
+
+[int]$operacion = Read-Host "Elije una opción"
+if ($operacion -ne 5){
+
+[double]$x = Read-Host "Dame un número"
+[double]$y = Read-Host "Dame otro número"
+
+
+Function Sumar {
+    param (
+        [double]$x,
+        [double]$y
+    )
+    $sumar = $x + $y
+    Write-Host "La respuesta es $sumar"
+}
+
+Function Restar {
+    param (
+        [double]$x,
+        [double]$y
+    )
+    $restar = $x - $y
+    Write-Host "La respuesta es $restar"
+}
+
+Function Multiplicar {
+    param (
+        [double]$x,
+        [double]$y
+    )
+    $multiplicar = $x * $y
+    Write-Host "La respuesta es $multiplicar"
+}
+
+Function Dividir {
+    param (
+        [double]$x,
+        [double]$y
+    )
+    $dividir = $x / $y
+    Write-Host "La respuesta es $dividir"
+}
+
+
+
+Switch ($operacion) {
+1{
+    Sumar $x $y
+    }
+2{
+
+    Restar $x $y
+    }
+3{
+
+    Multiplicar $x $y
+    }
+4{
+
+    Dividir $x $y
+    }
+default {
+    Write-Host "Has puesto un número fuera de la lista"
+}
+}
+}
---
+++ Practica5/ej2.ps1
@@ -0,0 +1,6 @@
+﻿$usuarios = Import-Csv Z:\mis_proyectos\ejercicios_powershell\Practica5\usuarios.csv -Delimiter ","
+
+foreach ($us in $usuarios)
+{
+ 	Write-Host "Usuario: $($us.nombre) $($us.apellidos) $($us.grupo)"
+}
