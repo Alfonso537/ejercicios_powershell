000cd45e9ad8b28a301d5feda5f46fbc391268b4
Alfonso <alfonsocastellperiz@gmail.com>
2023-10-27 12:15:42 +0200

    Practica6 acabada

---
+++ UT2_P6/script_ut2_p6_NTFS.ps1
@@ -0,0 +1,43 @@
+﻿Import-Module ActiveDirectory
+$empleados = Import-Csv C:\Users\Administrador\Desktop\empleados.csv -delimiter ";"
+
+
+#Crear la estructura
+New-Item -Path C:\Empresa_users -ItemType Directory
+New-SmbShare -Name Empresa_users$ -Path C:\Empresa_users -FullAccess "Usuarios del dominio"
+
+foreach ($emp in $empleados) {
+    $nombre = $emp.nombre
+    $apellido = $emp.apellido
+    $usuario = "$nombre.$apellido"
+    $ruta = "C:\Empresa_users\$usuario"
+    
+    New-Item -Path $ruta -ItemType Directory
+}
+
+
+#Permisos NTFS
+foreach ($emp in $empleados)
+{
+    $acl = Get-Acl -Path C:\Empresa_users\$($emp.nombre).$($emp.apellido)
+    # Deshabilitar la herencia y eliminar TODAS las reglas de acceso
+    $acl.SetAccessRuleProtection($true,$false)
+
+    $permisos = 'Administradores','FullControl','ContainerInherit,ObjectInherit','None','Allow'
+    $ace = New-Object System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisos
+    $acl.SetAccessRule($ace)
+
+    $permisos = "$($emp.nombre).$($emp.apellido)",'FullControl','ContainerInherit,ObjectInherit','None','Allow'
+    $ace = New-Object System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisos
+    $acl.SetAccessRule($ace)
+
+
+    $acl | Format-Table
+    Set-Acl -AclObject $acl -Path C:\Empresa_users\$($emp.nombre).$($emp.apellido)
+}
+
+
+foreach ($emp in $empleados)
+{
+Set-ADUser -Identity "$($emp.nombre).$($emp.apellido)" -ScriptPath "carpeta$($emp.departamento).bat" -HomeDrive "Z:" -HomeDirectory "\\empresa-DC1\Empresa_users$\$($emp.nombre).$($emp.apellido)"
+}
